b371fe8639e50208e0c528c0ccb77aeb2a93f0f5
Pedro Krapinski Neto <pedrokn@gmail.com>
2021-12-29 20:09:47 -0300

    commit

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+var navContainer = document.getElementById("navContainer");
+var openBtn = document.getElementById('openBtn');
+function openNav() {
+    navContainer.style.right = "0";
+    openBtn.style.display = "none";
+}
+
+function closeNav() {
+    navContainer.style.right = "-500px";
+    openBtn.style.display = "block";
+}
