5ed12b30b6faa8dc5424bb21443033dcb6a831b6
Pedro Krapinski Neto <91733572+KarpaNeto@users.noreply.github.com>
2021-12-30 12:31:29 -0300

    commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,10 @@ var navContainer = document.getElementById("navContainer");
 var openBtn = document.getElementById('openBtn');
 function openNav() {
     navContainer.style.right = "0";
-    openBtn.style.display = "none";
+    
 }
 
 function closeNav() {
     navContainer.style.right = "-500px";
-    openBtn.style.display = "block";
+    
 }
