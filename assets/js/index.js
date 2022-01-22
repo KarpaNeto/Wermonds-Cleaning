7bfd7c919534b1e0b1315f86cbce616da706b58e
Pedro Krapinski Neto <pedrokn@gmail.com>
2022-01-21 22:14:32 -0300

    commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,3 +58,20 @@ const toggleItem = (item) => {
     item.classList.add("accordion-open");
   }
 };
+
+
+
+
+
+/*  SCROLL UP */
+
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 400) {
+    scrollUp.classList.add('show-scroll'); 
+  } else {
+    scrollUp.classList.remove('show-scroll');
+  } 
+}
+window.addEventListener('scroll', scrollUp)
